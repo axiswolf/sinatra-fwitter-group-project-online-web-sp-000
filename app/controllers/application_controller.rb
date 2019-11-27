84458aa952b1fb081a7b4dd1e1f4746753dce7a2
Karen V <karenvo@usc.edu>
2019-11-27 06:43:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,86 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    set :session_secret, "secret" # sets encryption key for session
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  enable :sessions # need this to log in
+  enable :method_override
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if Helpers.logged_in?(session)
+      Helpers.current_user(session)
+      redirect to '/tweets'
+    else
+    #   redirect to '/signup'
+      erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+    # create user
+    # don't let user sign up without username, email, password
+    # @user = User.create(:username => params[:username], :password => params[:password])
+    #params = (:username => params["username"], :password => params["password"], :email => params["email"])
+    @user = User.new(username: params["username"], password: params["password"], email: params["email"])
+    #@user = User.new(params)
+    if @user.username == "" || @user.password == "" || @user.email == ""
+      redirect '/signup'  # redirects to signup page
+    else
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    end
+  end
+
+  get '/login' do
+    # loads all tweets after login (redirect)
+    # do not let users view login page if already logged in
+    if Helpers.logged_in?(session)
+      Helpers.current_user(session)
+      redirect to '/tweets'
+    else
+    #   redirect to '/signup'
+      erb :'/users/login'
+    end
+    # end
+    #erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to "/login"
+    end
+    redirect to "/login"
+  end
+
+  get '/logout' do
+    # lets a user logout if they are already logged in and redirects to the login page
+    # redirects a user to the index page if the user tries to access /logout while not logged in
+    # redirects a user to the login route if a user tries to access /tweets route if user is not logged in
+    if Helpers.logged_in?(session)
+      Helpers.current_user(session)
+      erb :'/users/logout'
+    else
+      redirect '/'
+    end
+      # session.clear
+      # redirect '/login'
+  end
+
+  post '/logout' do
+    session.clear
+    redirect '/login'
   end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,79 @@
+require 'sinatra/base'
+require 'rack-flash'
+#require_relative './controllers/helpers/helpers'
+
 class TweetsController < ApplicationController
+  enable :sessions
+  enable :method_override
 
+ use Rack::Flash
 
+  get '/tweets' do
+      if Helpers.logged_in?(session)
+          @user = Helpers.current_user(session)
+          @tweets = Tweet.all
+          @users = User.all
+          erb :'/tweets/tweets'
+      else
+          redirect to "/login"
+      end
+  end
+
+  get '/tweets/new' do
+    if Helpers.logged_in?(session)
+        @user = Helpers.current_user(session)
+        @tweets = Tweet.all
+        @users = User.all
+        erb :'/tweets/new'
+    else
+        redirect to "/login"
+    end
+  end
+
+  post '/tweets' do
+    #@user = user.id
+    @tweet = Tweet.new(content: params["content"], user_id: params["user_id"])
+    @tweet.save
+    redirect to "/tweets"
+  end
+
+  get '/tweets/:id' do
+    @user = Helpers.current_user(session)
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show'
+  end
+
+  delete '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    if @tweet && @tweet.destroy
+      redirect "/tweets"
+    else
+      redirect "/tweets/#{tweet.id}"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    @user = Helpers.current_user(session)
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/edit'
+  end
+#    redirect to "/tweets/#{@tweets.id}"
+  patch '/tweets/:id' do
+      @tweet = Tweet.find(params[:id])
+      @user = Helpers.current_user(session)
+        if @tweet.user_id == @user.id
+          if params["content"] != ""
+              @tweet.content = params["content"]
+              @tweet.save
+              flash[:message] = "Successfully updated tweet."
+              redirect to "/tweets/#{@tweet.id}/edit"
+          else
+              flash[:message] = "Your tweet cannot be empty."
+              redirect to "/tweets/#{@tweet.id}/edit"
+          end
+        else
+          flash[:message] = "You can only edit and delete your own tweets"
+          redirect to "/tweets"
+        end
+    end
 end
