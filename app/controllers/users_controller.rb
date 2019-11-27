aae186514d0d6a35312e71857122dcc3caa3794d
Karen V <karenvo@usc.edu>
2019-11-27 07:16:03 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
-  get "/users/:id" do
-    @user = User.find_by_id([:id])
-    @tweets = Tweet.find(user_id: params["id"])
-    erb :'/users/show'
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    @tweets = @user.tweets
+    erb :'users/show'
   end
 
 end
