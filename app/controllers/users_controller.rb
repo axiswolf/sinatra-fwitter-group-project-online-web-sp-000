95d70f5e63c4e1cdf154f8771c63c6b77be2eea6
Karen V <karenvo@usc.edu>
2019-11-27 07:04:23 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  get "/users/#{@user.slug}" do
+  get "/users/#{user.slug}" do
     user = User.find_by_id([:user_id])
     @tweets = Tweets.find(user_id: params[:id])
     erb :'/users/show'
