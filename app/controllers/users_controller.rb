5a90d86620efc3a81187a555cdc03eb02746d1f6
Karen V <karenvo@usc.edu>
2019-11-27 07:02:02 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   get '/users/:id' do
     #@user = User.find_by_id([:user_id])
-    tweets = Tweets.find(user_id: params[:id])
+    @tweets = Tweets.find(user_id: params[:id])
     erb :'/users/show'
   end
 
